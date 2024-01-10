19002fb4a8433eca1737df712f6b25a67dba6e94
Alexander Voros <alex.voros22@gmail.com>
2024-01-10 14:29:10 +0200

    added new implementation

--- src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -1,22 +1,23 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class ConstructorInjectedControllerTest {
 
     ConstructorInjectedController controller;
 
     @BeforeEach
     void setUp() {
-        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+        controller = new ConstructorInjectedController(new ConstructorGreetingService());
     }
 
     @Test
     void getGreeting() {
         System.out.println(controller.getGreeting());
+        String abc = "abcdef";
+        log.info("test {}",abc);
     }
 }
