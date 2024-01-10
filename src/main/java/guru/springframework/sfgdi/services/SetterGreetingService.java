81231d2f46b2c0522982f36918389c2d6513b3f7
Alexander Voros <alex.voros22@gmail.com>
2024-01-10 14:48:18 +0200

    added java configuration by using @configuration, moved bean configuration into the component class

--- src/main/java/guru/springframework/sfgdi/services/SetterGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterGreetingService.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.stereotype.Service;
 
-@Service
-public class SetterGreetingService implements GreetingService{
+public class SetterGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
         return "Hello world - Setter";
